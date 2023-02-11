d27858d44a4f0fefb31725175fdddbd3713e56d4
Maaruf Dauda <mkdauda.work@gmail.com>
2023-02-11 11:57:16 +0000

    Add startup helper script for application

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# docker-compose exec app rm -rf vendor composer.lock
+docker-compose exec app composer install
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan config:cache
+docker-compose exec app php artisan migrate
+docker-compose exec app php artisan db:seed > /dev/null
+# exit 0
